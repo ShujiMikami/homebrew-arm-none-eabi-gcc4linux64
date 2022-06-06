61865a15adce693bf488e45efcd8d48ef93a14e1
ShujiMikami <carbonized.pan@gmail.com>
2022-06-06 16:44:17 +0900

    ifだめ？

--- gcc-arm-none-eabi.rb
+++ gcc-arm-none-eabi.rb
@@ -3,11 +3,11 @@ require 'formula'
 class GccArmNoneEabi < Formula
   homepage "https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads"
   version '10.3-2021.10'
-  if OS.linux?
+  #if OS.linux?
     url "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2"
-  elseif OS.mac?
-    url "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-mac.tar.bz2"
-  end
+  #elseif OS.mac?
+  #  url "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-mac.tar.bz2"
+  #end
   #sha256 "97dbb4f019ad1650b732faffcc881689cedc14e2b7ee863d390e0a41ef16c9a3"
 
   def install
